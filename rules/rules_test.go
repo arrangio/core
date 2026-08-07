2bf3443ffdf34a277c02838e9c4ea8a59a518e9c
dvprokofiev <d@dvprokofiev.ru>
2026-08-07 20:27:37 +0300

    refactor: add missing file

--- rules/rules_test.go
+++ rules/rules_test.go
@@ -5,57 +5,17 @@ import (
 	"testing"
 
 	"arrangio-core/entity"
-	"arrangio-core/geometry"
 	"arrangio-core/grid"
 	"arrangio-core/rules"
-	"arrangio-core/tags"
 )
-
-// TestEntity is a "blank" for `buildTestEntity` function
-type TestEntity struct {
-	ID      uint64
-	Anchor  geometry.Point64
-	Tags    []int // tags that would be assigned to this entity
-	Facings *geometry.Facings
-	W, H, D int16 // 1x1x1 by default
-}
 
 // RuleTestCase describes the test case
 type RuleTestCase struct {
 	Name      string
-	Subject   TestEntity
-	Neighbors []TestEntity // slice of entities that would be placed to the grid
+	Subject   entity.TestEntity
+	Neighbors []entity.TestEntity // slice of entities that would be placed to the grid
 	Expected  float64
 	Tolerance float64
-}
-
-// buildTestEntity builds an `Entity` from `TestEntity`
-func buildTestEntity(cfg TestEntity) *entity.Entity {
-	mask := tags.NewMask()
-	for _, tagID := range cfg.Tags {
-		mask = mask.With(tagID)
-	}
-
-	w, h, d := cfg.W, cfg.H, cfg.D
-	if w == 0 {
-		w = 1
-	}
-	if h == 0 {
-		h = 1
-	}
-	if d == 0 {
-		d = 1
-	}
-
-	return &entity.Entity{
-		ID:   cfg.ID,
-		Tags: mask,
-		Footprint: geometry.Footprint{
-			Anchor:  cfg.Anchor,
-			Shape:   geometry.Box{W: w, H: h, D: d},
-			Facings: cfg.Facings,
-		},
-	}
 }
 
 func RunRuleTest(t *testing.T, rule rules.Rule, tc RuleTestCase) {
@@ -66,12 +26,12 @@ func RunRuleTest(t *testing.T, rule rules.Rule, tc RuleTestCase) {
 
 		// placing neighbors
 		for _, nCfg := range tc.Neighbors {
-			neighbor := buildTestEntity(nCfg)
+			neighbor := entity.BuildTestEntity(nCfg)
 			g.Insert(neighbor)
 		}
 
 		// building subject from `TestEntity`
-		subject := buildTestEntity(tc.Subject)
+		subject := entity.BuildTestEntity(tc.Subject)
 		g.Insert(subject)
 
 		ctx := &rules.RuleContext{
