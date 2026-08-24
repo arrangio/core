4950f79fe4bfbf15c909737f855551b7bb098acc
dvprokofiev <d@dvprokofiev.ru>
2026-08-24 11:07:39 +0300

    feat(zone): introduce `Zone` struct
    - `Zone` helps to turn raw `geometry.Footprint` to the object that solver and rules can work with
    - in the future it would be used to describe rules like 'certain Entity needs to be in a Zone...'

---
+++ zones/zone.go
@@ -0,0 +1,30 @@
+package zones
+
+import (
+	"arrangio-core/geometry"
+	"arrangio-core/tags"
+)
+
+type Zone struct {
+	ID          uint64
+	Name        string
+	Footprint   geometry.Footprint
+	Tags        tags.Mask
+	LastQueryID uint64
+}
+
+func (z *Zone) GetID() uint64 {
+	return z.ID
+}
+
+func (z *Zone) WorldBounds() (geometry.Point64, geometry.Point64) {
+	return z.Footprint.WorldBounds()
+}
+
+func (z *Zone) GetQueryID() uint64 {
+	return z.LastQueryID
+}
+
+func (z *Zone) SetQueryID(id uint64) {
+	z.LastQueryID = id
+}
